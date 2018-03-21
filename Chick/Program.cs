04df752753a93a0a82a18f7b75f342a83909ecb7
SeBastian <SeBastian@SEBASTAIN>
2018-03-21 17:27:02 +0800

    加入專案檔案。

---
+++ Chick/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chick
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int X=0;
+            int Y=0;
+            int Z=0;
+            for (int i = 0; i < 900/29;i++)
+            {
+               if((900-59*i)%29==0)
+                {
+                    X = i;
+                    Y = (900 - 54 * i) / 29;
+                    Z = 100 - X - Y;
+                    break;
+                }
+                 
+            }
+            Console.WriteLine("公雞"+X.ToString()+"母雞:"+Y.ToString()+"小雞:"+Z.ToString());
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ Prime/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 101; i <= 200; i++)
+            {
+                if (PrimeAll(i))
+                {
+                    Console.WriteLine("質數" +i);
+                }
+            }
+        }
+        static bool PrimeAll(int item)
+        {
+            for (int i = 2; i < item; i++)
+            {
+                if (item % i == 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
